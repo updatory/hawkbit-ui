be80c8152075ef56a794f70727b9a7aba341a74d
Tomasz Oponowicz <tomasz@zaxo.biz>
2024-03-06 08:46:00 +0100

    Reorganize the side bar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,43 +1,43 @@
 <template>
-  <div id="application-sidebar" class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700">
-    <div class="px-6">
-      <a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="hawkBit">hawkBit</a>
-    </div>
+  <div id="application-sidebar" class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700">
+    <div class="h-full flex flex-col justify-between px-6 py-4">
+      <div class="justify-start">
+        <div class="pb-6">
+          <a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="hawkBit">hawkBit</a>
+        </div>
 
-    <nav class="hs-accordion-group p-6 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
-      <ul class="space-y-1.5">
-        <SideBarLink label="Home" href="/">
+        <ul class="space-y-1.5">
+          <SideBarLink label="Rollouts" href="/rollouts">
+            <template #icon>
+              <RolloutIcon />
+            </template>
+          </SideBarLink>
+          <SideBarLink label="Targets" href="/targets">
+            <template #icon>
+              <TargetIcon />
+            </template>
+          </SideBarLink>
+          <SideBarLink label="Software Modules" href="/modules">
+            <template #icon>
+              <ModuleIcon />
+            </template>
+          </SideBarLink>
+          <SideBarLink label="Distribution Sets" href="/distributions">
+            <template #icon>
+              <DistributionIcon />
+            </template>
+          </SideBarLink>
+        </ul>
+      </div>
+
+      <ul>
+        <SideBarLink label="admin" href="/settings">
           <template #icon>
-            <HomeIcon />
-          </template>
-        </SideBarLink>
-        <SideBarLink label="Rollouts" href="/rollouts">
-          <template #icon>
-            <RolloutIcon />
-          </template>
-        </SideBarLink>
-        <SideBarLink label="Targets" href="/targets">
-          <template #icon>
-            <TargetIcon />
-          </template>
-        </SideBarLink>
-        <SideBarLink label="Software Modules" href="/modules">
-          <template #icon>
-            <ModuleIcon />
-          </template>
-        </SideBarLink>
-        <SideBarLink label="Distribution Sets" href="/distributions">
-          <template #icon>
-            <DistributionIcon />
-          </template>
-        </SideBarLink>
-        <SideBarLink label="Settings" href="/settings">
-          <template #icon>
-            <SettingsIcon />
+            <UserIcon />
           </template>
         </SideBarLink>
       </ul>
-    </nav>
+    </div>
   </div>
 </template>
 
@@ -46,7 +46,6 @@ import DistributionIcon from '@/icons/DistributionIcon.vue'
 import ModuleIcon from '@/icons/ModuleIcon.vue'
 import TargetIcon from '@/icons/TargetIcon.vue'
 import RolloutIcon from '@/icons/RolloutIcon.vue'
-import SettingsIcon from '@/icons/SettingsIcon.vue'
+import UserIcon from '@/icons/UserIcon.vue'
 import SideBarLink from '@/components/SideBarLink.vue'
-import HomeIcon from '@/icons/HomeIcon.vue'
 </script>
