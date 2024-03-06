8b2aabe1474960a4fd1afa5701d898caff980c08
Tomasz Oponowicz <tomasz@zaxo.biz>
2024-03-06 09:59:22 +0100

    Parametrize page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,30 +1,12 @@
 <template>
-  <header class="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-[48] w-full bg-white border-b text-sm py-2.5 sm:py-4 lg:ps-64 dark:bg-gray-800 dark:border-gray-700">
-    <nav class="flex basis-full items-center w-full mx-auto px-4 sm:px-6 md:px-8" aria-label="Global">
-      <div class="w-full flex items-center justify-between ms-auto sm:justify-between sm:gap-x-3 sm:order-3">
-        <div class="flex flex-row items-center justify-end gap-2">
-          <PrimaryButton label="New module">
-            <template #icon>
-              <PlusIcon />
-            </template>
-          </PrimaryButton>
-          <IconButton label="More">
-            <template #icon>
-              <MoreHorizontalIcon />
-            </template>
-          </IconButton>
-        </div>
-
-        <div class="flex justify-start gap-2 w-96">
-          <SearchInput />
-        </div>
-
-        <div class="flex flex-row items-center justify-end gap-2">
-          <SecondaryButton label="Filters">
-            <template #icon>
-              <FilterIcon />
-            </template>
-          </SecondaryButton>
+  <header class="sticky top-0 inset-x-0 sm:justify-start sm:flex-nowrap z-[48] w-full bg-white border-b text-sm py-4 dark:bg-gray-800 dark:border-gray-700">
+    <nav class="flex basis-full items-center w-full mx-auto px-4 sm:px-6 md:px-10" aria-label="Global">
+      <div class="w-full">
+        <div class="flex flex-row items-center justify-between gap-2">
+          <div class="flex flex-row items-center justify-start gap-2">
+            <h3 class="text-2xl dark:text-white">{{ title }}</h3>
+          </div>
+          <slot name="actions"></slot>
         </div>
       </div>
     </nav>
@@ -32,11 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import PrimaryButton from '@/components/PrimaryButton.vue'
-import IconButton from '@/components/IconButton.vue'
-import MoreHorizontalIcon from '@/icons/MoreHorizontalIcon.vue'
-import PlusIcon from '@/icons/PlusIcon.vue'
-import SecondaryButton from '@/components/SecondaryButton.vue'
-import FilterIcon from '@/icons/FilterIcon.vue'
-import SearchInput from '@/components/SearchInput.vue'
+const props = defineProps({
+  title: { type: String, required: true },
+})
 </script>
--- src/views/ModulesView.vue
+++ src/views/ModulesView.vue
@@ -1,10 +1,21 @@
 <template>
-  <PageHeader />
-  <div class="w-full pt-10 px-4 sm:px-6 md:px-8 lg:ps-72">
-    <h1>Modules</h1>
+  <div class="w-full lg:ps-64">
+    <PageHeader title="Software Modules">
+      <template #actions>
+        <PrimaryButton label="New module">
+          <template #icon>
+            <PlusIcon />
+          </template>
+        </PrimaryButton>
+      </template>
+    </PageHeader>
+    <DataTable />
   </div>
 </template>
 
 <script setup lang="ts">
 import PageHeader from '@/components/PageHeader.vue'
+import DataTable from '@/components/DataTable.vue'
+import PrimaryButton from '@/components/PrimaryButton.vue'
+import PlusIcon from '@/icons/PlusIcon.vue'
 </script>
